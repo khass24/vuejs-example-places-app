1a146b5a69a219b9d8dc9f4d6d84544c4dc3eb6f
Kyle Hassett <kylehassett72@gmail.com>
2018-10-18 21:08:19 -0500

    in class commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ headline }}</h1>
+  <ul>
+    <li v-for="error in errors">{{ error }}</li>
+  </ul>
+  <div>
+    Place: <input v-model="newPlace.name">
+    Address: <input v-model="newPlace.address">
+    <button v-on:click="addPlace()">Add Place</button>
+  </div>
+
+  <div v-for="place in places">
+    <h3> {{ place.name}}</h3>
+    <p> {{ place.address }}</p>
+  </div>
+
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+var axios = require("axios");
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      headline: "My Places",
+
+      places: [],
+
+      newPlace: {name: "", address: ""},
+      errors: []
+    };
+  },
+  created: function() {
+    axios
+    .get("http://localhost:3000/api/places")
+    .then(response => {
+      this.places = response.data;
+    });
+  },
+  methods: {
+    addPlace: function() {
+      this.errors = []
+      var params = {
+                    name: this.newPlace.name,
+                    address: this.newPlace.address
+                    };
+
+    axios
+    .post("http://localhost:3000/api/places", params)
+    .then(response => {
+      this.places.push(response.data);
+      this.newPlace = {name: "", address: ""}
+    })
+    .catch(error => {
+      this.errors = error.response.data.errors;
+    });
+
+    }
+  },
+  computed: {}
+};
 </script>
